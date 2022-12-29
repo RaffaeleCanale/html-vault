5f72625e3480f7e770b6a5f1f17a0aa05f30e226
Raffaele Canale <raffaelecanale@gmail.com>
2022-12-29 12:02:16 +0000

    Minor bugfix

--- src/resources/VaultTemplate.ts
+++ src/resources/VaultTemplate.ts
@@ -1,5 +1,5 @@
 export async function fetchTemplate(): Promise<string> {
-    const response = await fetch('./vault-template');
+    const response = await fetch('/vault-template');
 
     if (!response.ok) {
         throw new Error('Failed to fetch template');
