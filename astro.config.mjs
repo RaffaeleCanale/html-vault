f0081fee2f68c218365360730a35f337733760af
Raffaele Canale <raffaelecanale@gmail.com>
2022-12-29 12:17:47 +0000

    Inline CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import svelte from '@astrojs/svelte';
+import singleFile from 'astro-single-file';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte()],
+    integrations: [svelte(), singleFile()],
 });
